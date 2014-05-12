124ce2ef22e176cc18e102e634bcb037d7cb8f19
chu4lif3 <liang.chusen@gmail.com>
2014-05-12 09:31:50 -0400

    Filters added
    Added filters, created navigation bar items for camera, photo album

--- DateAppClickableDemo/DateAppClickableDemo/TWAppDelegate.m
+++ DateAppClickableDemo/DateAppClickableDemo/TWAppDelegate.m
@@ -13,6 +13,8 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+    [[UINavigationBar appearance] setTintColor:[UIColor blackColor]];
+
     // Override point for customization after application launch.
     [Parse setApplicationId:@"OoHIkXP4veqn7ch2kLv5UojFwQzyFC37QJDBaoir"
                   clientKey:@"ZdZ8sonuTHkAYzKWwQSu2EFBH1ThPNgl1PklDd5C"];
--- DateAppClickableDemo/DateAppClickableDemo/TWCreateTabViewController.h
+++ DateAppClickableDemo/DateAppClickableDemo/TWCreateTabViewController.h
@@ -8,42 +8,54 @@
 
 #import <UIKit/UIKit.h>
 
-
 @interface TWCreateTabViewController : UIViewController <UIImagePickerControllerDelegate,UINavigationControllerDelegate,UIActionSheetDelegate>
+{
+    CIContext *context;
+    NSMutableArray *filters;
+    CIImage *beginImage;
+    UIScrollView *filtersScrollView;
+    UIView *selectedFilterView;
+    UIImage *finalImage;
+    
+}
 
-@property (weak, nonatomic) IBOutlet UIButton *addPic;
-@property (strong, nonatomic) IBOutlet UIImageView *imageView;
+
+//@property (weak, nonatomic) IBOutlet UIButton *addPic;
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
 @property (weak, nonatomic) IBOutlet UISegmentedControl *chooseLocation;
 @property (weak, nonatomic) IBOutlet UITextField *locations;
 
-- (IBAction)album:(UIButton *)sender;
-- (IBAction)addPicture:(UIButton *)sender;
+//- (IBAction)album:(UIButton *)sender;
+//- (IBAction)addPicture:(UIButton *)sender;
 - (IBAction)submit:(UIButton *)sender;
-- (IBAction)retakePic:(UIButton *)sender;
+//- (IBAction)retakePic:(UIButton *)sender;
 - (IBAction)locationToggle:(UISegmentedControl *)sender;
 -(IBAction)ReturnKeyButton:(id)sender;
 
+- (IBAction)camera:(id)sender;
+- (IBAction)photoalbum:(id)sender;
 
 //overlay
-- (IBAction)done:(id)sender;
-- (IBAction)takePhoto:(id)sender;
-- (IBAction)startStop:(id)sender;
-- (IBAction)timedTakePhoto:(id)sender;
+//- (IBAction)done:(id)sender;
+//- (IBAction)takePhoto:(id)sender;
+//- (IBAction)startStop:(id)sender;
+//- (IBAction)timedTakePhoto:(id)sender;
 
 
-@property (weak, nonatomic) IBOutlet UIView *cameraOverlay;
+//@property (weak, nonatomic) IBOutlet UIView *cameraOverlay;
 
-@property (weak, nonatomic) IBOutlet UIBarButtonItem *cancelButton;
-@property (weak, nonatomic) IBOutlet UIBarButtonItem *takePictureButton;
-@property (weak, nonatomic) IBOutlet UIBarButtonItem *timedButton;
-@property (weak, nonatomic) IBOutlet UIBarButtonItem *startStopButton;
+
+//@property (weak, nonatomic) IBOutlet UIBarButtonItem *cancelButton;
+//@property (weak, nonatomic) IBOutlet UIBarButtonItem *takePictureButton;
+//@property (weak, nonatomic) IBOutlet UIBarButtonItem *timedButton;
+//@property (weak, nonatomic) IBOutlet UIBarButtonItem *startStopButton;
 
 //timers, if using it
-@property (nonatomic, retain) NSTimer *tickTimer;
-@property (nonatomic, retain) NSTimer *cameraTimer;
+//@property (nonatomic, retain) NSTimer *tickTimer;
+//@property (nonatomic, retain) NSTimer *cameraTimer;
 
 
-@property (nonatomic, retain) UIImagePickerController *imagePickerController;
+//@property (nonatomic, retain) UIImagePickerController *imagePickerController;
 
 
 @end
